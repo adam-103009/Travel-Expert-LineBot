3deb3fd108c280e2a5467f8cd85e0c4cd609b339
adam <20120s494@gmail.com>
2022-01-02 07:00:49 +0800

    Img

--- utils.py
+++ utils.py
@@ -27,7 +27,8 @@ def send_button_message(reply_token, title, text, btn):
         template = ButtonsTemplate(
             title = title,
             text = text,
-            actions = btn
+            actions = btn,
+            thumbnailImageUrl="https://lh3.googleusercontent.com/proxy/cbC-pT9oRgfisa75Z_JA3FOBwzxrEJEMt1ElrtQD4LXSNaePPyjRAjyIjAQoKt0Laalke0kjUySo_kwloJyy1aap3cLcrZL9lUUbRTnf6C5f6b61LL5FTl8ZS5NLZI8aBg"
         )
     )
     line_bot_api.reply_message(reply_token, message)
@@ -58,12 +59,12 @@ def send_image_message(reply_token, url):
     )
     line_bot_api.reply_message(reply_token, message)
 def get_url(url):
-    res="輸入“返回”可回到地區選單\n旅遊資訊"
+    res="輸入“返回”可回到地區選單\n旅遊資訊\n"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     titles = soup.find_all("h3", itemprop="headline")
     for title in titles:
-        res=res+title.select_one("a").getText()+'\n'+title.select_one("a").get("href")+'\n'
+        res='\n'+res+title.select_one("a").getText()+'\n'+title.select_one("a").get("href")+'\n'
     return res
 """
 def send_image_url(id, img_url):
